6aaca6a467e98829612c7f312b47d3b8bd4b26e4
Aayudh P Panchal <aayudhpanchal@gmail.com>
2024-11-11 23:19:26 +0530

    Introduction Page

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import IntroductionInteractiveElement from "../components/IntroductionInteractiveElement";
 
 const Introduction = () => {
   return (
-    <div>Introduction</div>
-  )
-}
+    <div className="main">
+      <div className="container flex items-center justify-center mx-40 flex-col text-center space-y-11">
+        <div className="hero flex items-center justify-center flex-col gap-3">
+          <img
+            src="/assets/logo2.png"
+            alt="Some error occurred"
+            className="md:w-80"
+          />
+          <p className="text-9xl font-bold tracking-tighter text-[#e8f3fe]">
+            DINO GRAPHICS
+          </p>
+        </div>
 
-export default Introduction
+        <div className="interactiveElement">
+          <IntroductionInteractiveElement />
+        </div>
+
+        <div className="navigator flex text-white">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+          neque natus magnam iusto praesentium consequatur dolor. Repudiandae et
+          aut nisi officia! Omnis dignissimos similique nostrum consequatur
+          natus!
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
